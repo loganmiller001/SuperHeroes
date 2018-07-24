06ccce3a360f23693e70e7be153af5888b11e4c4
loganmiller001 <loganmiller001@live.com>
2018-07-24 13:05:20 -0500

    Renamed the model class to SuperHero, removed an issue where it would call the SuperHeroes controller. Finsihed logic in create and update methods in controller.

--- SuperHeroes/Controllers/SuperHeroesController.cs
+++ SuperHeroes/Controllers/SuperHeroesController.cs
@@ -23,18 +23,29 @@ namespace SuperHeroes.Controllers
 
         [AcceptVerbs]
         [HttpPost]
-        public ActionResult Create(SuperHeroes superHeroes)
+        public ActionResult Create(SuperHero superHero)
         {
             if (ModelState.IsValid)
             {
+                db.SuperHeroes.Add(superHero);
                 db.SaveChanges();
             }
 
-            return View(superHeroes);
+            return View(superHero);
         }
 
-        public ActionResult Update()
+        public ActionResult Update(SuperHero superHero)
         {
+            SuperHero updatedHero = (from s in db.SuperHeroes
+                                     where s.SuperId == superHero.SuperId
+                                     select s).FirstOrDefault();
+            updatedHero.Name = superHero.Name;
+            updatedHero.PrimaryPower = superHero.PrimaryPower;
+            updatedHero.SecondaryPower = superHero.SecondaryPower;
+            updatedHero.AlterEgo = superHero.AlterEgo;
+            updatedHero.CatchPhrase = superHero.CatchPhrase;
+            db.SaveChanges();
+
             return View();
         }
 
